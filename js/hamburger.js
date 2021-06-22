8eebb14029b620fdffb9847df3cd5c872875fdbe
今村一貴 <imamurakazuki@imamurakazutakanoMacBook-Pro.local>
2021-06-22 18:53:41 +0900

    css

---
+++ js/hamburger.js
@@ -0,0 +1,11 @@
+$(function() {
+  
+  
+  $(".is-hamburger").click(function() {
+   
+     $(".p-hamburger").toggleClass("is-open");
+     $(".p-dropdown-menu__list").toggleClass("is-open");
+    
+  })
+  
+});
---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+$(function () {
+    $('.is-hamburger').click(function () {
+        $('.p-dropdown-menu__list').slideToggle();
+    });
+});
+jQuery(function ($) {
+
+    //質問をクリック
+    $(".c-faq__titile").click(function () {
+
+
+
+
+        $(".c-faq__titile").not(this).next().slideUp(300);
+
+
+
+
+        $(this).next().slideToggle(300);
+
+
+    });
+});
+
