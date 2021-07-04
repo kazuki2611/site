9258621ff98706947ee3628c918e30f291c52875
今村一貴 <imamurakazuki@imamurakazutakanoMacBook-Pro.local>
2021-07-04 09:50:27 +0900

    css

--- js/hamburger.js
+++ js/hamburger.js
@@ -7,5 +7,21 @@ $(function() {
      $(".p-dropdown-menu__list").toggleClass("is-open");
     
   })
+  // 画面リサイズでリロードさせる設定
+  var timer = 0;
+  var currentWidth = window.innerWidth;
+$(window).resize(function(){
+      if (currentWidth == window.innerWidth) {
+          return;
+      }
+      if (timer > 0) {
+          clearTimeout(timer);
+      }
+
+      timer = setTimeout(function () {
+          location.reload();
+      }, 200);
   
 });
+});
+
--- js/script.js
+++ js/script.js
@@ -1,28 +1,53 @@
 
 $(function () {
-    $('.is-hamburger').click(function () {
-        $('.p-dropdown-menu__list').slideToggle();
-    });
+  $('.is-hamburger').click(function () {
+    $('.p-dropdown-menu__list').slideToggle();
+  });
 });
 jQuery(function ($) {
 
-    //質問をクリック
-    $(".c-faq__titile").click(function () {
+  //質問をクリック
+  $(".c-faq__titile").click(function () {
 
 
 
 
-        $(".c-faq__titile").not(this).next().slideUp(300);
+    $(".c-faq__titile").not(this).next().slideUp(300);
 
 
 
 
-        $(this).next().slideToggle(300);
+    $(this).next().slideToggle(300);
 
 
+  });
+  $(window).resize(function () {
+    $(".p-dropdown-menu__list").css('display', '');
+  });
+});
+$(document).ready(function () {
+
+  $('#form').submit(function (event) {
+    var formData = $('#form').serialize();
+    $.ajax({
+      url: "https://docs.google.com/forms/hogehoge",
+      data: formData,
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
+          $(".end-message").slideDown();
+          $(".submit-btn").fadeOut();
+          //window.location.href = "thanks.html";
+        },
+        200: function () {
+          $(".false-message").slideDown();
+        }
+      }
     });
-    $(window).resize(function() {
-        $(".p-dropdown-menu__list").css('display','');
-    });
+    event.preventDefault();
+  });
+
+  
 });
 
